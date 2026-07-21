d2a9eba453a6c19e192a2fcecb912900dfc520be
Yang xiaoyu <85976753+sddzcuigc@users.noreply.github.com>
2026-07-21 22:10:52 +0800

    feat(blocktype): wire Phaser game bootstrap

---
+++ apps/blocktype-adventure/src/main.ts
@@ -0,0 +1,20 @@
+import Phaser from 'phaser';
+import './style.css';
+import { GameScene } from './game/GameScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 1280,
+  height: 720,
+  backgroundColor: '#8ed7ff',
+  pixelArt: true,
+  roundPixels: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [GameScene],
+};
+
+new Phaser.Game(config);
